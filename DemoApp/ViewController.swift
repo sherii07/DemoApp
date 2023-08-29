d2417b2d64b3bc17dc3b1d8ca3238955b46a70b3
Ali Sher <developersherii@gmail.com>
2023-08-29 11:38:09 +0500

    ViewWill..

--- DemoApp/ViewController.swift
+++ DemoApp/ViewController.swift
@@ -11,9 +11,11 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
     }
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
 
 }
 
